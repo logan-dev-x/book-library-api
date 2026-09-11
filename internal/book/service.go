5839a8462d6f5522fafae0ff88cfa55520f9dc0e
Logan <johncost7@proton.me>
2026-09-11 14:53:05 -0300

    feat: add book service

---
+++ internal/book/service.go
@@ -0,0 +1,40 @@
+package book
+
+import "errors"
+
+type Service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) Service {
+	return Service{repo: repo}
+}
+
+func (s Service) Create(book CreateBookInput) (Book, error) {
+	created, err := s.repo.Save(book)
+	if err != nil {
+		return Book{}, err
+	}
+	return created, nil
+}
+
+func (s Service) GetAll() []Book {
+	return s.repo.GetAll()
+}
+
+func (s Service) GetOne(id int) (Book, error) {
+	book, err := s.repo.GetByID(id)
+	if err != nil {
+		return Book{}, err
+	}
+	return book, nil
+}
+
+func (s Service) Update(id int, book UpdateBookInput) error {
+	b, err := s.repo.GetByID(id)
+	if err != nil && b == (Book{}) {
+		return errors.New("book not found")
+	}
+
+	return s.repo.Update(b.ID, book)
+}
