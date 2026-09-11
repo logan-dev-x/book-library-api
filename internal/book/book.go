54e997c3d95cc20cbdb63bd86b6ad75501c52b3d
Logan <johncost7@proton.me>
2026-09-11 13:42:12 -0300

    feat: add struct for inputs

--- internal/book/book.go
+++ internal/book/book.go
@@ -13,3 +13,16 @@ type Book struct {
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 }
+
+type CreateBookInput struct {
+	Title       string
+	Author      string
+	ISBN        string
+	Description string
+	PublishedAt time.Time
+}
+
+type UpdateBookInput struct {
+	Title       string
+	Description string
+}
